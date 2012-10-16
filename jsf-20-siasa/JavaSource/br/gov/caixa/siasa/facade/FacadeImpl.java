b4eca25644ec0c4328ef997d8e6d6afe1cba7f01
holiveiraz@gmail.com <holiveiraz@gmail.com@d497e310-2eda-476e-35bd-f71ff6719417>
2012-10-16 01:40:06 +0000

    Atualização e inclusão de novos documentos.

--- jsf-20-siasa/JavaSource/br/gov/caixa/siasa/facade/FacadeImpl.java
+++ jsf-20-siasa/JavaSource/br/gov/caixa/siasa/facade/FacadeImpl.java
@@ -1,64 +1,58 @@
 package br.gov.caixa.siasa.facade;
 
-import br.gov.caixa.siasa.model.dao.Asabk304CicsDAO;
-import br.gov.caixa.siasa.model.dao.Asabk307CicsDAO;
-import br.gov.caixa.siasa.model.dao.Asabk308CicsDAO;
-import br.gov.caixa.siasa.model.dao.CicsDAO;
+import br.gov.caixa.siasa.model.dao.CicsDaoFactory;
 import br.gov.caixa.siasa.model.dao.IDao;
 import br.gov.caixa.siasa.model.dto.CobolBook;
 
 public class FacadeImpl implements IFacade {
 
-	private IDao dao;
+	private IDao dao = null;
 	
 	@Override
 	public void consultaLancamentoDoacaoEmCheque(final CobolBook cb) {
-		dao = new CicsDAO("ASAPO301");
+		dao = CicsDaoFactory.getDao("MOCKPO301");// ASAPO301
 		dao.execute(cb);
 	}
 
 	@Override
 	public void consultaLancamentoEstornoPendente(final CobolBook cb) {
-		dao = new CicsDAO("ASAPO302");
+		dao = CicsDaoFactory.getDao("MOCKPO302");// ASAPO302
 		dao.execute(cb);
 	}
 
 	@Override
 	public void inclusaoEstornoCheque(final CobolBook cb) {
-		dao = new CicsDAO("ASAPO303");
+		dao = CicsDaoFactory.getDao("MOCKPO303");// ASAPO303
 		dao.execute(cb);
 	}
 
 	@Override
 	public void delecaoEstornoJaComandado(final CobolBook cb) {
-//		dao = new CicsDAO("ASAPO304");
-		dao = new Asabk304CicsDAO("ASAPO304");
+		dao = CicsDaoFactory.getDao("MOCKPO304");// ASAPO304
 		dao.execute(cb);
 	}
 
 	@Override
 	public void consultaLancamentoCheque(final CobolBook cb) {
-		dao = new CicsDAO("ASAPO305");
+		dao = CicsDaoFactory.getDao("MOCKPO305");// ASAPO305
 		dao.execute(cb);
 	}
 
 	@Override
 	public void consultaApuracaoRealizada(final CobolBook cb) {
-		dao = new CicsDAO("ASAPO306");
+		dao = CicsDaoFactory.getDao("MOCKPO306");// ASAPO306
 		dao.execute(cb);
 	}
 
 	@Override
 	public void consultaRemessaJaRealizada(final CobolBook cb) {
-//		dao = new CicsDAO("ASAPO307");
-		dao = new Asabk307CicsDAO("ASAPO307");
+		dao = CicsDaoFactory.getDao("MOCKPO307");// ASAPO307
 		dao.execute(cb);
 	}
 
 	@Override
 	public void manutencaoTipoCancal(final CobolBook cb) {
-//		dao = new CicsDAO("ASAPO308");
-		dao = new Asabk308CicsDAO("ASAPO308");
+		dao = CicsDaoFactory.getDao("MOCKPO308");// ASAPO308
 		dao.execute(cb);
 	}
 
